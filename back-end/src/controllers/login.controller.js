d22e87011a96cea8c36c932dde1e403824b007a4
marcelsguima <marcelsguima@gmail.com>
2023-05-23 18:14:41 -0300

    Fix: login error message

--- back-end/src/controllers/login.controller.js
+++ back-end/src/controllers/login.controller.js
@@ -10,8 +10,9 @@ const createUser = async (req, res) => {
     return res.status(400).json({ message: 'Some required fields are missing' }); 
   }
   const newUser = await loginService.login(email, password);
+  console.log(newUser);
   if (!newUser) {
-    return res.status(404).json({ message: 'Invalid fields' });
+    return res.status(404).json({ message: 'User not found' });
   }
   const toToken = newUser.dataValues;
   const token = generateToken(toToken);
